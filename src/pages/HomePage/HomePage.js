44cfb311f2248bb7a9f230640f6a6aff87ae23c2
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-01-18 00:45:06 -0700

    implement name search

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
+import Fuse from 'fuse.js'
 
 import { PokemonContext } from '../App/App.js'
 import Card from '../../components/Card/Card.js'
@@ -11,15 +12,31 @@ const CardContainer = styled.div`
       justify-content: center;
   `;
 
+
 export default function HomePage() {
+    
+    const [input, setInput] = useState('')
 
     const allPokemon = useContext(PokemonContext);
-
+    
+    const fuse = new Fuse(allPokemon, {
+        keys: [
+            'name'
+        ],
+        includeScore: true
+    });
+    const nameSearch = fuse.search(input);
+    const nameSearchResults = input ? nameSearch.map(result => result.item) : allPokemon;
+    
+    const handleChange = (e) => {
+          setInput(e.target.value);
+      }
 
     return (
         <div>
+            <span>Search By Name: <input placeholder="Pikachu" type="text" value={input} onChange={handleChange} /></span>
             <CardContainer>
-                {allPokemon.map(p => (
+                {nameSearchResults.map(p => (
                     <Card
                         pokemon={p}
                         key={p.id}
