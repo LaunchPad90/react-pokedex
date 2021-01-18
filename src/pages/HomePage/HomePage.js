b0db3407bbbde8ba6b5779d3e00d46a1da2d2258
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-01-18 01:19:48 -0700

    type filter search working with name search

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import Fuse from 'fuse.js'
+import intersection from "lodash.intersection"
+import uniq from "lodash.uniq"
+import Select from 'react-select'
 
 import { PokemonContext } from '../App/App.js'
 import Card from '../../components/Card/Card.js'
@@ -12,29 +15,62 @@ const CardContainer = styled.div`
       justify-content: center;
   `;
 
+const getFormattedTypeOptions = (allPokemon) => {
+    const typeOptions = [];
+     
+    allPokemon.forEach((pokemon) => {
+        typeOptions.push(pokemon.type);
+    })
+
+    const allTypeOptions = typeOptions.flat();
+
+    return uniq(allTypeOptions).map((option) => {
+        return {value: option, label: option};
+    })
+}
 
 export default function HomePage() {
     
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+    const [typeFilters, setTypeFilters] = useState([]);
 
     const allPokemon = useContext(PokemonContext);
+
+    const formattedTypeOptions = getFormattedTypeOptions(allPokemon);
+
+    const filterByType = (pokemon) => {
+        const flatTypeList = (typeFilters || []).map((typeOption) => typeOption.value)
+        return intersection(flatTypeList, pokemon.type).length === flatTypeList.length;
+    }
+
+    const filteredByType = allPokemon.filter(filterByType);
     
-    const fuse = new Fuse(allPokemon, {
+    const fuse = new Fuse(filteredByType, {
         keys: [
             'name'
         ],
         includeScore: true
-    });
+    })
+
     const nameSearch = fuse.search(input);
-    const nameSearchResults = input ? nameSearch.map(result => result.item) : allPokemon;
+    const nameSearchResults = input ? nameSearch.map(result => result.item) : filteredByType;
     
     const handleChange = (e) => {
           setInput(e.target.value);
       }
-
+      
+      const typeFilterChange = (value) => {
+          setTypeFilters(value);
+      }
+      
     return (
         <div>
             <span>Search By Name: <input placeholder="Pikachu" type="text" value={input} onChange={handleChange} /></span>
+
+            <Select options={formattedTypeOptions} onChange={typeFilterChange} isMulti />
+            
+
+
             <CardContainer>
                 {nameSearchResults.map(p => (
                     <Card
