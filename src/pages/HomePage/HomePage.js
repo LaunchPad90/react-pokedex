6b91db96e490b51f0e30d5f386704c399d4b4313
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-01-18 01:33:10 -0700

    search filter for weaknesses

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -33,6 +33,7 @@ export default function HomePage() {
     
     const [input, setInput] = useState('');
     const [typeFilters, setTypeFilters] = useState([]);
+    const [weaknessesFilters, setWeaknessesFilters] = useState([]);
 
     const allPokemon = useContext(PokemonContext);
 
@@ -43,7 +44,13 @@ export default function HomePage() {
         return intersection(flatTypeList, pokemon.type).length === flatTypeList.length;
     }
 
+    const filterByWeaknesses = (pokemon) => {
+        const flatWeaknessList = (weaknessesFilters || []).map((weaknessOption) => weaknessOption.value)
+        return intersection(flatWeaknessList, pokemon.weaknesses).length === flatWeaknessList.length
+    }
+
     const filteredByType = allPokemon.filter(filterByType);
+    const filteredByWeaknesses = filteredByType.filter(filterByWeaknesses);
     
     const fuse = new Fuse(filteredByType, {
         keys: [
@@ -53,23 +60,25 @@ export default function HomePage() {
     })
 
     const nameSearch = fuse.search(input);
-    const nameSearchResults = input ? nameSearch.map(result => result.item) : filteredByType;
+    const nameSearchResults = input ? nameSearch.map(result => result.item) : filteredByWeaknesses;
     
     const handleChange = (e) => {
           setInput(e.target.value);
       }
       
-      const typeFilterChange = (value) => {
-          setTypeFilters(value);
-      }
+    const typeFilterChange = (value) => {
+        setTypeFilters(value);
+    }
+    const weaknessFilterChange = (value) => {
+        setWeaknessesFilters(value);
+    }
       
     return (
         <div>
             <span>Search By Name: <input placeholder="Pikachu" type="text" value={input} onChange={handleChange} /></span>
 
             <Select options={formattedTypeOptions} onChange={typeFilterChange} isMulti />
-            
-
+            <Select options={formattedTypeOptions} onChange={weaknessFilterChange} isMulti />
 
             <CardContainer>
                 {nameSearchResults.map(p => (
