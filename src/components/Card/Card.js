a78b796dfc4c8b035e4fdbcfc641f8339cd3c664
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-01-18 00:35:20 -0700

    very basic card styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,24 +4,35 @@ import styled from 'styled-components'
 const Div = styled.div`
     margin: 30px;
     width: 250px;
-    height: 200px;
+    height: 350px;
+    border-radius: 10px;
     text-align: center;
-    border: 3px solid black;
+    border: 7px solid #e4bc46;
+    background: #5fa8d3;
 
     .name {
         font-size: 30px;
         text-decoration: none;
-        color: red;
+    }
+    .num {
+        font-size: 12px;
+    }
+    .photo {
+        border: 3px solid #fec601;
+        border-radius: 7px;
+        background: #fec601;
+        width: 70%;
     }
 `;
 
 export default function Card({pokemon}) {
-    console.log(pokemon.name)
     return (
         <Div>
             <div className="name">{pokemon.name}</div>
             <div className="num">{pokemon.num}</div>
             <img className="photo" alt={pokemon.name} src={pokemon.img} />
+            <div className="type">Type:{pokemon.type}</div>
+            <div className="weakness">Weakness:{pokemon.weaknesses}</div>
         </Div>
     )
 }
